b982ab42055dc6a60c98154c3b0210b5901e13ba
Dina Dehbi <dinadh012@gmail.com>
2024-11-22 19:03:06 +0100

    edite depo secondly

--- resources/views/articles/show.blade.php
+++
@@ -1,8 +0,0 @@
-
-@extends('layouts.app')
-
-@section('content')
-    <h1>{{ $article->title }}</h1>
-    <p>{{ $article->content }}</p>
-    <a href="{{ route('articles.index') }}">Retour à la liste</a>
-@endsection
--- resources/views/components/article-card.blade.php
+++
@@ -1,5 +0,0 @@
-<div class="article-card">
-    <h3>{{ $article->title }}</h3>
-    <p>{{ Str::limit($article->content, 100) }}</p>
-    <a href="{{ route('articles.show', $article->id) }}">Lire la suite</a>
-</div>
