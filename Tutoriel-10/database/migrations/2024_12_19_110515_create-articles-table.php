0a293fc3494afa48c80b91cbdc71f711f9574270
Dina Dehbi <dinadh012@gmail.com>
2024-12-19 12:25:23 +0100

    Tutoriel-10

---
+++ Tutoriel-10/database/migrations/2024_12_19_110515_create-articles-table.php
@@ -0,0 +1,27 @@
+<?php
+
+
+// database/migrations/xxxx_xx_xx_xxxxxx_create_articles_table.php
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArticlesTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('content');
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('category_id')->constrained('categories')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
